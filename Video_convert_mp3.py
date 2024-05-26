0c69c5e8bbb718073f0dfe98841311abde3fd237
Ayush Babu <132811238+Ayush231002@users.noreply.github.com>
2024-05-27 01:32:58 +0530

    Add files via upload

---
+++ Video_convert_mp3.py
@@ -0,0 +1,8 @@
+import moviepy.editor
+from tkinter.filedialog import*
+
+vid = askopenfilename()
+video = moviepy.editor.VideoFileClip(vid)
+aud=video.audio
+aud.write_audiofile("audio.mp3")
+print("__END__")
